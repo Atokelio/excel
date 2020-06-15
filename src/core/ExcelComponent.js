6dada7646bb29cce0595ebad098c51f632e494fe
bsafonov <bsafonov@webim.ru>
2020-06-15 13:20:38 +0300

    start table-logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,6 +4,11 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+
+    this.prepere()
+  }
+
+  prepere() {
   }
 
   // возвращает HTML шаблон компонента
